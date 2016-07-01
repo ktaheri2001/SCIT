c2984b82ce71685299c332f64c6af830c9310b99
ktaheri2001 <k.taheri2001@gmail.com>
2016-07-01 18:15:29 +0430

    Delete Classification.m

--- Codes/Classification.m
+++
@@ -1,9 +0,0 @@
-clc;clear;close all
-load Records
-tra_bo = 10;
-feature = 20;
-SVMStruct = svmtrain(Records(1:tra_bo,1:16),Records(1:tra_bo,feature));
-testLabels = svmclassify(SVMStruct,Records(tra_bo+1:end,1:16));
-results =  testLabels == Records(tra_bo+1:end,feature) ;
-CCR = sum(results)/size(results,1);
-CCR
